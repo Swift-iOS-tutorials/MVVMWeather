ad7759a49ac6f6cbf8a3a904407134df2e3e1c87
Dennis Shar <dennis.shar@gmail.com>
2022-08-06 15:59:22 +0300

    view model connected to view

--- MVVMWeather/Models.swift
+++ MVVMWeather/Models.swift
@@ -8,3 +8,19 @@
 import Foundation
 
 //The actual data
+
+struct WeatherModel:Codable {
+    let weather: [WeatherInfo]
+    let main: Main
+    let name:String
+}
+
+struct WeatherInfo:Codable{
+    let main:String
+    let description:String
+}
+
+struct Main:Codable {
+    let temp:Float
+}
+
--- MVVMWeather/ViewModel.swift
+++ MVVMWeather/ViewModel.swift
@@ -15,4 +15,35 @@ class WeatherViewModel: ObservableObject{
     @Published var description: String = "-"
     @Published var temp: String = "-"
     @Published var timezone: String = "-"
+    
+    init(){
+        fetchWeather()
+    }
+    
+    func fetchWeather(){
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=32.0853&lon=34.7818&units=metric&appid=aa2df23d347d91a01f286584e35f2b7e") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) {data,_,error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            //convert data to model
+            do {
+                let model = try JSONDecoder().decode(WeatherModel.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.title = model.weather.first?.main ?? "No Title"
+                    self.description = model.weather.first?.description ?? "No Description"
+                    self.temp =  "\(model.main.temp)°"
+                    self.timezone = model.name
+                }
+                
+            }catch{
+                print("Failed")
+            }
+        }
+        task.resume()
+    }
 }
